ea436ba92bf864eff46cabdde7b7851963429cda
Andrew W <marwat208@gmail.com>
2017-11-26 23:09:53 -0500

    Remove Unused Imports

--- src/main/java/flaxbeard/cyberware/common/item/ItemNeuropozyne.java
+++ src/main/java/flaxbeard/cyberware/common/item/ItemNeuropozyne.java
@@ -5,17 +5,13 @@ import java.util.List;
 import com.mojang.realmsclient.gui.ChatFormatting;
 
 import net.minecraft.client.resources.I18n;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -34,14 +30,21 @@ public class ItemNeuropozyne extends Item
 		this.setMaxDamage(0);
 		CyberwareContent.items.add(this);
 	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
 	{
-		if (!playerIn.capabilities.isCreativeMode)
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		if(!playerIn.capabilities.isCreativeMode)
 		{
 			stack.shrink(1);
 		}
+
 		playerIn.addPotionEffect(new PotionEffect(CyberwareContent.neuropozyneEffect, 24000, 0, false, false));
+		return new ActionResult<>(EnumActionResult.SUCCESS, stack);
 
 	}
+
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced)
 	{
