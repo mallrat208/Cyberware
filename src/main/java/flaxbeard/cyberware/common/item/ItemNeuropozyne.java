df833196ccae2a8624cd8d7326cb32cbf9905b80
Andrew W <marwat208@gmail.com>
2017-11-26 23:04:27 -0500

    Correct onItemRightClick for ItemNeuropozyne
    Should now work properly.

--- src/main/java/flaxbeard/cyberware/common/item/ItemNeuropozyne.java
+++ src/main/java/flaxbeard/cyberware/common/item/ItemNeuropozyne.java
@@ -13,7 +13,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -28,27 +30,18 @@ public class ItemNeuropozyne extends Item
 		this.setRegistryName(name);
 		GameRegistry.register(this);
 		this.setUnlocalizedName(Cyberware.MODID + "." + name);
-		
 		this.setCreativeTab(Cyberware.creativeTab);
-				
 		this.setMaxDamage(0);
-
 		CyberwareContent.items.add(this);
 	}
-	
-	
-	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand)
 	{
 		if (!playerIn.capabilities.isCreativeMode)
 		{
 			stack.shrink(1);
 		}
-		
 		playerIn.addPotionEffect(new PotionEffect(CyberwareContent.neuropozyneEffect, 24000, 0, false, false));
 
-		return new ActionResult(EnumActionResult.SUCCESS, stack);
 	}
-	
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced)
 	{
