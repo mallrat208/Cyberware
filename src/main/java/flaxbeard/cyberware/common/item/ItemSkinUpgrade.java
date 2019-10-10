65256261a83b3d8c74aea9c7486d4821ec37ea6e
Unknown <lemadec.fr@gmail.com>
2019-10-10 10:40:37 +0200

    Fixed #114 immunosuppressant not always affecting poison and hunger

--- src/main/java/flaxbeard/cyberware/common/item/ItemSkinUpgrade.java
+++ src/main/java/flaxbeard/cyberware/common/item/ItemSkinUpgrade.java
@@ -1,5 +1,6 @@
 package flaxbeard.cyberware.common.item;
 
+import javax.annotation.Nonnull;
 import java.util.*;
 
 import net.minecraft.enchantment.EnchantmentThorns;
@@ -8,6 +9,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
@@ -84,9 +86,8 @@ public class ItemSkinUpgrade extends ItemCyberware
 		return MathHelper.cos(celestialAngleRadians2);
 	}
 	
-	private Set<UUID> setIsImmunosuppressantPowered = new HashSet<>();
-	private static Map<UUID, Collection<PotionEffect>> mapPotions = new HashMap<>();
-
+	private static Map<UUID, Collection<LastPotionEffect>> mapEntityLastPotionEffects = new HashMap<>();
+	
 	@SubscribeEvent(priority = EventPriority.HIGH)
 	public void handleMissingEssentials(CyberwareUpdateEvent event)
 	{
@@ -96,65 +97,89 @@ public class ItemSkinUpgrade extends ItemCyberware
 		ItemStack itemStackImmunosuppressant = cyberwareUserData.getCyberware(getCachedStack(META_IMMUNOSUPPRESSANT));
 		if (!itemStackImmunosuppressant.isEmpty())
 		{
-			boolean isPowered = entityLivingBase.ticksExisted % 20 == 0
-			                  ? cyberwareUserData.usePower(itemStackImmunosuppressant, getPowerConsumption(itemStackImmunosuppressant))
-			                  : setIsImmunosuppressantPowered.contains(entityLivingBase.getUniqueID());
-			
-			if ( !isPowered
-			  && entityLivingBase instanceof EntityPlayer
-			  && entityLivingBase.ticksExisted % 100 == 0
-			  && !entityLivingBase.isPotionActive(CyberwareContent.neuropozyneEffect) )
+			// consume power every 1 s, apply damage every 5 s unless power or Neuropozyne is active
+			if ( entityLivingBase instanceof EntityPlayer
+			  && entityLivingBase.ticksExisted % 20 == 0 )
 			{
-				entityLivingBase.attackEntityFrom(EssentialsMissingHandler.lowessence, 2F);
-			}
-			
-			if (mapPotions.containsKey(entityLivingBase.getUniqueID()))
-			{
-				Collection<PotionEffect> potionsLastActive = mapPotions.get(entityLivingBase.getUniqueID());
-				Collection<PotionEffect> currentEffects = entityLivingBase.getActivePotionEffects();
-				for (PotionEffect potionEffectCurrent : currentEffects)
+				boolean isPowered = cyberwareUserData.usePower(itemStackImmunosuppressant, getPowerConsumption(itemStackImmunosuppressant));
+				
+				if ( !isPowered
+				  && entityLivingBase.ticksExisted % 100 == 0
+				  && !entityLivingBase.isPotionActive(CyberwareContent.neuropozyneEffect) )
 				{
-					if ( potionEffectCurrent.getPotion() == MobEffects.POISON
-					  || potionEffectCurrent.getPotion() == MobEffects.HUNGER )
-					{
-						boolean found = false;
-						for (PotionEffect potionEffectLast : potionsLastActive)
-						{
-							if ( potionEffectLast.getPotion() == potionEffectCurrent.getPotion()
-							  && potionEffectLast.getAmplifier() == potionEffectCurrent.getAmplifier() )
-							{
-								found = true;
-								break;
-							}
-						}
-						
-						if (!found)
-						{
-							entityLivingBase.addPotionEffect(new PotionEffect(potionEffectCurrent.getPotion(),
-							                                                  (int) (potionEffectCurrent.getDuration() * 1.8F),
-							                                                  potionEffectCurrent.getAmplifier(),
-							                                                  potionEffectCurrent.getIsAmbient(),
-							                                                  potionEffectCurrent.doesShowParticles() ));
-						}
-					}
+					entityLivingBase.attackEntityFrom(EssentialsMissingHandler.lowessence, 2.0F);
 				}
 			}
 			
-			if (isPowered)
+			// increase poison and hunger duration
+			if (!entityLivingBase.getEntityWorld().isRemote)
 			{
-				setIsImmunosuppressantPowered.add(entityLivingBase.getUniqueID());
+				Collection<LastPotionEffect> lastPotionEffects = mapEntityLastPotionEffects.get(entityLivingBase.getUniqueID());
+				if (lastPotionEffects == null)
+				{// (this is our first time seeing this player)
+					// save all current poison and hunger potions
+					lastPotionEffects = new ArrayList<>(2);
+					Collection<PotionEffect> currentEffects = entityLivingBase.getActivePotionEffects();
+					for (PotionEffect potionEffectCurrent : currentEffects)
+					{
+						if ( potionEffectCurrent.getPotion() == MobEffects.POISON
+						  || potionEffectCurrent.getPotion() == MobEffects.HUNGER )
+						{
+							lastPotionEffects.add(new LastPotionEffect(potionEffectCurrent));
+						}
+					}
+					mapEntityLastPotionEffects.put(entityLivingBase.getUniqueID(), lastPotionEffects);
+				}
+				else
+				{
+					// mark last potion effects for removal
+					lastPotionEffects.forEach(lastPotionEffect -> lastPotionEffect.isFound = false);
+					
+					// check all current poison and hunger potions
+					Collection<PotionEffect> currentEffects = entityLivingBase.getActivePotionEffects();
+					for (PotionEffect potionEffectCurrent : currentEffects)
+					{
+						if ( potionEffectCurrent.getPotion() == MobEffects.POISON
+						  || potionEffectCurrent.getPotion() == MobEffects.HUNGER )
+						{
+							// check if it's a new one or changed
+							boolean found = false;
+							for (LastPotionEffect lastPotionEffect : lastPotionEffects)
+							{
+								if ( lastPotionEffect.potion    == potionEffectCurrent.getPotion()
+								  && lastPotionEffect.amplifier == potionEffectCurrent.getAmplifier()
+								  && lastPotionEffect.duration  >= potionEffectCurrent.getDuration() )
+								{
+									lastPotionEffect.isFound = true;
+									found = true;
+									break;
+								}
+							}
+							
+							if (!found)
+							{
+								final PotionEffect potionEffectAugmented = new PotionEffect(potionEffectCurrent.getPotion(),
+								                                                            (int) (potionEffectCurrent.getDuration() * 1.8F),
+								                                                            potionEffectCurrent.getAmplifier(),
+								                                                            potionEffectCurrent.getIsAmbient(),
+								                                                            potionEffectCurrent.doesShowParticles() );
+								entityLivingBase.addPotionEffect(potionEffectAugmented);
+								final LastPotionEffect lastPotionEffectAugmented = new LastPotionEffect(potionEffectAugmented);
+								lastPotionEffectAugmented.isFound = true;
+								lastPotionEffects.add(lastPotionEffectAugmented);
+							}
+						}
+					}
+					
+					// update and remove last potion effects
+					lastPotionEffects.forEach(lastPotionEffect -> lastPotionEffect.duration--);
+					lastPotionEffects.removeIf(lastPotionEffect -> lastPotionEffect.duration < 0 || !lastPotionEffect.isFound);
+				}
 			}
-			else
-			{
-				setIsImmunosuppressantPowered.remove(entityLivingBase.getUniqueID());
-			}
-			
-			mapPotions.put(entityLivingBase.getUniqueID(), entityLivingBase.getActivePotionEffects());
 		}
 		else if (entityLivingBase.ticksExisted % 20 == 0)
 		{
-			setIsImmunosuppressantPowered.remove(entityLivingBase.getUniqueID());
-			mapPotions.remove(entityLivingBase.getUniqueID());
+			mapEntityLastPotionEffects.remove(entityLivingBase.getUniqueID());
 		}
 	}
 	
@@ -196,5 +221,27 @@ public class ItemSkinUpgrade extends ItemCyberware
 	public int getPowerProduction(ItemStack stack)
 	{
 		return stack.getItemDamage() == META_SOLARSKIN ? LibConstants.SOLAR_PRODUCTION : 0;
+	}
+	
+	private static class LastPotionEffect
+	{
+		public final Potion potion;
+		public final int amplifier;
+		public int duration;
+		public boolean isFound;
+		
+		LastPotionEffect(@Nonnull PotionEffect potionEffect)
+		{
+			potion = potionEffect.getPotion();
+			amplifier = potionEffect.getAmplifier();
+			duration = potionEffect.getDuration();
+			isFound = false;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("%s x %d, Duration:  %d",
+			                     potion.getName(), amplifier + 1, duration );
+		}
 	}
 }
